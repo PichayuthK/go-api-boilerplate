7a1e084385d6544878db907ed233d513a38cdf72
pichayuthk <pichayuthkitti@gmail.com>
2022-11-13 20:40:27 +0700

    add comment

--- business/repository/todo/todo.go
+++ business/repository/todo/todo.go
@@ -1,10 +1,12 @@
 package repository
 
+// Todo struct represent a Todo model in a scope of database
 type Todo struct {
 	ID    int    `db:"todo_id"`
 	Title string `db:"title"`
 }
 
+// TodoRepository list all functionalities the are needed to be supported by TodoRepository
 type TodoRepository interface {
 	ListTodo() ([]Todo, error)
 	GetTodo(id int) (*Todo, error)
